af67ccbb17e4a11927681e811e27fd2479922e6c
_Eckermania <127853724+Eckermania514@users.noreply.github.com>
2026-01-22 17:03:19 -0500

    Simplify shutdown script for Rocket League
    Removed VBS message box code and added a timeout message.

--- main.bat
+++ main.bat
@@ -1,16 +1,4 @@
 @echo off
-SET MessageTitle=RL Remote Shutdown
-SET MessageBody=RL will close in 10 minutes
-SET TempVBS=%TEMP%\\popup.vbs
-
-REM Create the VBS file with the message box code
-ECHO x=MsgBox("%MessageBody%", 0, "%MessageTitle%") > "%TempVBS%"
-
-REM Run the VBS file using WScript
-WScript "%TempVBS%"
-
-REM Delete the temporary VBS file
-DEL "%TempVBS%"
-
+echo Closing RL in 5 minutes... (space to skip)
 timeout /t 600 > nul
 TASKKILL /F /IM "RocketLeague.exe"
