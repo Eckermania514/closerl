89a783ba79dfc890962993b09c51e9282a430282
_Eckermania <127853724+Eckermania514@users.noreply.github.com>
2026-01-22 17:01:41 -0500

    Update installation script to create scripts directory

--- installation.bat
+++ installation.bat
@@ -2,6 +2,9 @@
 color 02
 echo Installation will begin in 15 seconds. Press any key to skip
 timeout /t 15 > nul
+IF NOT EXIST "C:\scripts\" (
+    MD "C:\scripts"
+)
 curl https://raw.githubusercontent.com/Eckermania514/closerl/refs/heads/main/startup.bat -o "C:\Users\%USERNAME%\AppData\Roaming\Microsoft\Windows\Start Menu\Programs\Startup"
 curl https://raw.githubusercontent.com/Eckermania514/closerl/refs/heads/main/bot.py -o "C:\scripts"
 curl https://raw.githubusercontent.com/Eckermania514/closerl/refs/heads/main/main.bat -o "C:\scripts"
